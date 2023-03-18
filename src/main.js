6ac846d2551db115d0173afb4506ff27748a2b07
Anil <karnamanil2000@gmail.com>
2023-03-18 17:57:49 +0530

    3-18-2023: Project set up completed

--- src/main.js
+++ src/main.js
@@ -3,4 +3,21 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+// Bootstrap, Jquery, Popper
+import "@popperjs/core/dist/umd/popper";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery/src/jquery.js";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(store).use(router).use(vuetify).mount("#app");
